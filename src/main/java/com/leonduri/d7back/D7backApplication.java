1756a108710fe576094dc24cbcb3e560ccc2e05a
jhp2060 <jhp2060@gmail.com>
2022-04-16 23:54:11 +0900

    initial commit

---
+++ src/main/java/com/leonduri/d7back/D7backApplication.java
@@ -0,0 +1,13 @@
+package com.leonduri.d7back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D7backApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D7backApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leonduri/d7back/D7backApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonduri.d7back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D7backApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
