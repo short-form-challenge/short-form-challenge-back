6cae200620ac3c6c3751d8260706b8607f7fbd14
jhp2060 <jhp2060@gmail.com>
2022-04-17 21:50:06 +0900

    feat(vo): newly created User VO(entity)

---
+++ src/main/java/com/leonduri/d7back/User/User.java
@@ -0,0 +1,82 @@
+package com.leonduri.d7back.User;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.Collection;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isAdmin;
+
+    @Column
+    private String profileFilePath;
+
+    @Column
+    private Date createdAt;
+
+    @Column
+    private Date lastLogin;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
