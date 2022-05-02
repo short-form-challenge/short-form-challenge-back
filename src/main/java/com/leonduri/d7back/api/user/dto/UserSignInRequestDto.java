c320d091ebd57695fb20fe218e1d6b3669b9f2d8
jhp2060 <jhp2060@gmail.com>
2022-05-03 00:01:50 +0900

    refactor(dto): update example of usersinginrequestdto in swagger

--- src/main/java/com/leonduri/d7back/api/user/dto/UserSignInRequestDto.java
+++ src/main/java/com/leonduri/d7back/api/user/dto/UserSignInRequestDto.java
@@ -1,11 +1,16 @@
 package com.leonduri.d7back.api.user.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class UserSignInRequestDto {
+
+    @ApiModelProperty(value = "이메일", example = "required/string", required = true)
     private final String email;
+
+    @ApiModelProperty(value = "비밀번호", example = "required/string", required = true)
     private final String password;
 
     @Builder
