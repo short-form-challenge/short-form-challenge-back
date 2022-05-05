4bb6db85db8d8c24f5a3ed80c2c604d4801fe71e
jhp2060 <jhp2060@gmail.com>
2022-05-05 23:57:30 +0900

    feat(advice): new error handling - missing request part/param, wrong media format, http message not readable

--- src/main/java/com/leonduri/d7back/utils/ExceptionAdvice.java
+++ src/main/java/com/leonduri/d7back/utils/ExceptionAdvice.java
@@ -3,13 +3,19 @@ package com.leonduri.d7back.utils;
 import com.leonduri.d7back.utils.exception.CEmailSignInFailedException;
 import com.leonduri.d7back.utils.exception.CInvalidJwtTokenException;
 import com.leonduri.d7back.utils.exception.CUserNotFoundException;
+import com.leonduri.d7back.utils.exception.CWrongMediaFormatException;
 import io.jsonwebtoken.MalformedJwtException;
+import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -53,4 +59,29 @@ public class ExceptionAdvice {
     protected ApiResponse invalidJwtToken(HttpServletRequest request, CInvalidJwtTokenException e) {
         return ApiResponse.fail(CInvalidJwtTokenException.errorMsg);
     }
+
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ApiResponse missingRequestPart(HttpServletRequest request, MissingServletRequestPartException e) {
+        return ApiResponse.fail("request part 중 필수 값이 입력되지 않았습니다.");
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ApiResponse missingRequestParameter(HttpServletRequest request, MissingServletRequestParameterException e) {
+        return ApiResponse.fail("request parameter 중 필수 값이 입력되지 않았습니다.");
+    }
+
+    @ExceptionHandler(CWrongMediaFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ApiResponse wrongMediaFormat(HttpServletRequest request, CWrongMediaFormatException e) {
+        return ApiResponse.fail(CWrongMediaFormatException.errorMsg);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ApiResponse httpMessageNotReadable(HttpServletRequest request, HttpMessageNotReadableException e) {
+        return ApiResponse.fail("JSON 형식이 맞지 않아 파싱할 수 없거나 request message 중 형식 오류가 있습니다.");
+    }
+
 }
