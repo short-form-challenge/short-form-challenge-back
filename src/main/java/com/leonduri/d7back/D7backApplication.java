2dafc50c6d923ce32a66c2a39689faca69a91201
jhp2060 <jhp2060@gmail.com>
2022-05-13 23:15:42 +0900

    feat(etc): resource handler config

--- src/main/java/com/leonduri/d7back/D7backApplication.java
+++ src/main/java/com/leonduri/d7back/D7backApplication.java
@@ -1,15 +1,21 @@
 package com.leonduri.d7back;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class D7backApplication {
+
+    @Value("${spring.servlet.multipart.docker-location}")
+    private String multifilePath;
 
     public static void main(String[] args) {
         SpringApplication.run(D7backApplication.class, args);
@@ -30,7 +36,13 @@ public class D7backApplication {
                         .allowedOrigins("*")
                         .exposedHeaders("X-AUTH-TOKEN", "REFRESH-TOKEN");
             }
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                ResourceHandlerRegistration resourceHandlerRegistration = registry.addResourceHandler("/videos/**")
+                        .addResourceLocations(multifilePath)
+                        .setCachePeriod(20);
+            }
         };
     }
-
 }
+    
