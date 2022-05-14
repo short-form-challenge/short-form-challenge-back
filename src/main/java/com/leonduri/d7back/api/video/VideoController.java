97d33fc859202de0e0284d9898a92239c1bf30c2
jhp2060 <jhp2060@gmail.com>
2022-05-14 20:24:23 +0900

    refactor(controller): return api response when one or more videos are deleted

--- src/main/java/com/leonduri/d7back/api/video/VideoController.java
+++ src/main/java/com/leonduri/d7back/api/video/VideoController.java
@@ -56,12 +56,12 @@ public class VideoController {
         return SingleApiResponse.success(videoService.findVideoById(videoId, userId));
     }
 
-    //TODO 파일 삭제 추가 필요
     @ApiOperation(value = "비디오 삭제", notes = "비디오 id를 기준으로 비디오 하나를 삭제한다.")
     @DeleteMapping(value = "/videos/{videoId}")
-    public void deleteVideoById(
+    public ApiResponse deleteVideoById(
             @PathVariable @ApiParam(value = "비디오 Id", required = true) long videoId) throws Exception {
         videoService.deleteVideoById(videoId);
+        return ApiResponse.success("해당 video " + videoId + "를 삭제하였습니다.");
     }
 
     //    userId 임시
@@ -279,9 +279,10 @@ public class VideoController {
 
     @ApiOperation(value = "Admin page : 유저의 비디오 삭제", notes = "유저 정보로 전체 비디오 삭제")
     @DeleteMapping(value = "/admin/{userId}")
-    public void deleteVideosByPostedBy(
+    public ApiResponse deleteVideosByPostedBy(
             @PathVariable @ApiParam(value = "유저 Id", required = true) long userId) throws Exception {
         videoService.deleteVideosByPostedBy(userId);
+        return ApiResponse.success("해당 user " + userId + "가 게시한 비디오를 모두 삭제하였습니다.");
     }
 
     //        API 두개 호출 필요
