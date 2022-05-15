3c88f8fd9a1bb929ad1edb4e631dbf5a0ea78e03
jhp2060 <jhp2060@gmail.com>
2022-05-15 23:29:09 +0900

    fix(repository): case sensitive bug in SQL

--- src/main/java/com/leonduri/d7back/api/video/VideoRepository.java
+++ src/main/java/com/leonduri/d7back/api/video/VideoRepository.java
@@ -39,7 +39,7 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
             "where video.show_id >= :showId order by video.show_id, video.id desc limit :count", nativeQuery = true)
     List<Video> getMyVideoList(@Param("userId") Long userId, @Param("showId")Long showId, @Param("count") Integer count);
 
-    @Query(value = "SELECT * FROM video join User on video.posted_by = user.id " +
+    @Query(value = "SELECT * FROM video join user on video.posted_by = user.id " +
             "join category on video.category_id = category.id and video.category_id = :categoryId " +
             "where video.show_id >= :showId order by video.show_id, video.id desc limit :count", nativeQuery = true)
     List<Video> getMainVideoList(@Param("categoryId") Long categoryId, @Param("showId")Long showId, @Param("count") Integer count);
@@ -49,7 +49,7 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
             "where video.show_id >= :showId order by video.show_id, video.id desc limit :count", nativeQuery = true)
     List<Video> getAnonymousUserMain(@Param("categoryId") Long categoryId, @Param("showId")Long showId, @Param("count") Integer count);
 
-    @Query(value = "SELECT * FROM video join User " +
+    @Query(value = "SELECT * FROM video join user " +
             "join likes on likes.liked_by = :userId and likes.liked_on = video.id and user.id = :userId " +
             "where video.show_id >= :showId order by video.show_id, video.id desc limit :count", nativeQuery = true)
     List<Video> getLikedVideoList(@Param("userId") Long userId, @Param("showId")Long showId, @Param("count") Integer count);
@@ -57,10 +57,10 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
     //Admin API
     @Modifying
     @Transactional
-    @Query(value = "delete from video where video.posted_by = :userId" , nativeQuery = true)
+    @Query(value = "delete from video where video.posted_by = :userId", nativeQuery = true)
     void deleteVideosByPostedBy(@Param("userId") Long postedBy);
 
-    @Query(value = "SELECT * FROM video join User on video.posted_by = user.id and video.posted_by = :userId " +
+    @Query(value = "SELECT * FROM video join user on video.posted_by = user.id and video.posted_by = :userId " +
             "order by video.id limit :count offset :page", nativeQuery = true)
     List<Video> getAdminUserVideoList(@Param("userId") Long userId, @Param("page")Long page, @Param("count") Integer count);
 
